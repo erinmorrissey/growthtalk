820a483d1747777f9d62ce9515c00c892751a9e6
Erin Morrissey <erinmorrissey@gmail.com>
2016-10-22 15:32:25 +0000

    Add validation & tests for 'category' model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  it 'is valid with valid attributes' do
+    expect(Category.new(name: 'test category')).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    cat = Category.new(name: nil)
+    expect(cat).to_not be_valid
+  end
 end
