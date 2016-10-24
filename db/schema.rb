303d85e0b5de006ae0ce0f87fb82bc0e9ff12202
Erin Morrissey <erinmorrissey@gmail.com>
2016-10-24 16:27:44 +0000

    Add category_id to 'resources' model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022160649) do
+ActiveRecord::Schema.define(version: 20161024162340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,11 @@ ActiveRecord::Schema.define(version: 20161022160649) do
     t.string   "name"
     t.string   "url"
     t.string   "logo_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
+
+  add_index "resources", ["category_id"], name: "index_resources_on_category_id", using: :btree
 
 end
