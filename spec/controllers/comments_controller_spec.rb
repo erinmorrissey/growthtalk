f17e424c413e0d3b6007dfb4258bf40479aa44ea
Erin Morrissey <erinmorrissey@gmail.com>
2016-10-28 20:01:03 +0000

    Configure create action for comments

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :controller do
+  let(:res) { Resource.create(name: 'test resource', category_id: 57) }
+
+  describe 'comments#create' do
+    it 'creates a new comment in the DB' do
+      post :create, resource_id: res.id, comment: { comment_text: 'test comment' }
+      comment = Comment.last
+      expect(comment.comment_text).to eq 'test comment'
+      expect(comment.resource_id).to eq res.id
+      expect(response).to redirect_to resource_path(res)
+    end
+
+    it 'returns a 404 error - if the resource is NOT found' do
+      post :create, resource_id: 'TACOCAT', comment: { comment_text: 'test comment' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+end
