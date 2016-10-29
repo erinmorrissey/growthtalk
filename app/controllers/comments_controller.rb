7228ce1a5c3c0266bfd35d7873366f164cc8c0a0
Erin Morrissey <erinmorrissey@gmail.com>
2016-10-29 23:10:46 +0000

    Merge current user with comment params upon comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,10 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @resource = Resource.find_by(id: params[:resource_id])
     return render text: 'Not Found :(', status: :not_found if @resource.blank?
-    @resource.comments.create(comment_params)
+    @resource.comments.create(comment_params.merge(user: current_user))
     redirect_to resource_path(@resource)
   end
 
