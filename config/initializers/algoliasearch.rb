34feffca4f0a1b529746ed3df1d7e6adf7362b2d
Erin Morrissey <erinmorrissey@gmail.com>
2016-11-04 15:05:01 +0000

    Add Figaro & configure Algolia env vars

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,4 @@
-AlgoliaSearch.configuration = { application_id: "G1FKOVL9DQ", api_key: "4b884f922b42a3e0b9cc67eff45e2cf6" }
+AlgoliaSearch.configuration = {
+  application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'],
+  api_key: ENV['ALGOLIASEARCH_API_KEY']
+}
