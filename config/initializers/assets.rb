188a2c3d30f39ab7025b094319342b6daa612c19
Erin Morrissey <erinmorrissey@gmail.com>
2016-11-06 18:56:57 +0000

    Initial service worker configurations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,10 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += [
+  'custom.js',
+  'jquery.raty.js',
+  'ratyrate.js',
+  'master.css',
+  'search.css'
+]
